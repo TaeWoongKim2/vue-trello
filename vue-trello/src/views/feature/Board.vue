b19d915950d6786917ec2b8c6353ef251e07a3dc
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-08-17 16:23:19 +0900

    Vue Router Completed

--- vue-trello/src/views/feature/Board.vue
+++ vue-trello/src/views/feature/Board.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="board">
-    Board
-    <p>Board Id: {{ bid }}</p>
-    <ul>
-      <li><router-link :to="`/b/${bid}/c/1`">Card Name#1</router-link></li>
-      <li><router-link :to="`/b/${bid}/c/2`">Card Name#2</router-link></li>
-      <li><router-link :to="`/b/${bid}/c/3`">Card Name#3</router-link></li>
-    </ul>
+    Board - (ID : {{ bid }})
+    <div v-if="loading">Board Loading...</div>
+    <div v-else>
+      <ul>
+        <li><router-link :to="`/b/${bid}/c/1`">Card Name#1</router-link></li>
+        <li><router-link :to="`/b/${bid}/c/2`">Card Name#2</router-link></li>
+        <li><router-link :to="`/b/${bid}/c/3`">Card Name#3</router-link></li>
+      </ul>
+    </div>
     <hr>
     <router-view/>
   </div>
@@ -21,13 +23,24 @@ export default {
     
   },
   created() {
-    this.bid = this.$route.params.bid;
+    this.fetchData();
   },
   data() {
     return {
-      bid: 0
+      bid: 0,
+      loading: false,
     }
-  }
+  },
+  methods: {
+    fetchData() {
+      // API에서 데이터를 받아서 data()객체에 추가하는 역할을 담당하는 Method.
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      }, 1000);
+    }
+  },
 }
 </script>
 
--- vue-trello/src/views/feature/Card.vue
+++ vue-trello/src/views/feature/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="card">
     Card
-    <p>Card Id: {{ cid }}</p>
+    <div v-if="loading">Card Loading...</div>
+    <div v-else>
+      <p>Card Id: {{ cid }}</p>
+    </div>
   </div>
 </template>
 
@@ -14,18 +17,31 @@ export default {
     
   },
   watch: {
-    // 와쳐(watcher)를 통해 라우터의 URL 변화를 감지하고, ID값을 변경시킬 수 있다!
-    '$route'() {
-      this.cid = this.$route.params.cid;
+    // 감시자(watcher)를 통해 라우터의 URL 변화를 감지하고, ID값을 변경시킬 수 있다!
+    '$route': {
+      // this.fetchCard(); --> (Better Coding)
+      // 객체 형태로 변경하면, created()와의 중복 코드를 피할 수 있다!
+      handler: "fetchCard",
+      immediate: true
     }
   },  
   created() {
-    this.cid = this.$route.params.cid;
+    // this.fetchCard();
   },
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false,
     }
-  }
+  },
+  methods: {
+    fetchCard() {
+      this.loading = true;
+      setTimeout(() => {
+        this.cid = this.$route.params.cid;
+        this.loading = false;
+      }, 500);
+    }
+  },
 }
 </script>
